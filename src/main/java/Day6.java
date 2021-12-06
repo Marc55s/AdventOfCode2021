20b4ea8b604d9d506d61e67cb8ae4df0a1089df8
Marc <marc.schoenig@gmail.com>
2021-12-06 06:57:45 +0100

    Day 6

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -1,3 +1,5 @@
+import javax.swing.text.MaskFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Day6 extends AOCHandler{
@@ -7,6 +9,25 @@ public class Day6 extends AOCHandler{
 	}
 
 	void solve(List<String> input) {
+		List<Integer> lanternFish = new ArrayList<>();
+		String[] split = input.get(0).split(",");
+		for(String s : split){
+			lanternFish.add(Integer.parseInt(s));
+		}
+		System.out.println(lanternFish.size()
+		);
+		for (int i = 0; i < 80; i++) {
+			for (int j = 0; j < lanternFish.size(); j++) {
+				if(lanternFish.get(j) == 0){
+					lanternFish.set(j,6);
+					lanternFish.add(9);
+				}else {
+					lanternFish.set(j, lanternFish.get(j) - 1);
+				}
+			}
+		}
+		System.out.println("Part 1:"+lanternFish.size());
+
 
 	}
 }
