8e9ccea461816d10cae67645eab68b9972917dd8
Marc <marc.schoenig@gmail.com>
2021-12-07 18:36:33 +0100

    Day 6 Refactor

--- src/main/java/Day6.java
+++ src/main/java/Day6.java
@@ -10,17 +10,15 @@ public class Day6 extends AOCHandler {
         super("6");
     }
 
-    long[] fish;
+
 
     void solve(List<String> input) {
         List<Integer> lanternFish = new ArrayList<>();
-        int[] startTimer = new int[9];
         String[] split = input.get(0).split(",");
         for (String s : split) {
             lanternFish.add(Integer.parseInt(s));
         }
-        fish = new long[9];
-
+        long[] fish = new long[9];
         lanternFish.forEach(i -> fish[i]++);
         for (int i = 0; i < 80; i++) {
             for (int j = 0; j < lanternFish.size(); j++) {
@@ -34,13 +32,12 @@ public class Day6 extends AOCHandler {
         }
         System.out.println("Part 1: " + lanternFish.size());
         int days = 256;
-        for (int i = 0; i < 256; i++) {
+        for (int i = 0; i < days; i++) {
             long newFish = fish[0];
             System.arraycopy(fish, 1, fish, 0, fish.length - 1);
             fish[6] += newFish;
             fish[8] = newFish;
         }
-
         System.out.println("Part 2: "+Arrays.stream(fish).sum());
     }
 
