62e40e9d2a511fcfcc508c794d3a6322c407a10f
Marc <marc.schoenig@gmail.com>
2022-08-13 01:40:03 +0200

    Day 8 solution

--- src/main/java/Day8.java
+++ src/main/java/Day8.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day8 extends AOCHandler {
 
     public Day8() {
-        super("test");
+        super("8");
     }
 
     void solve(List<String> input) {
@@ -12,7 +13,9 @@ public class Day8 extends AOCHandler {
         int c = 0;
         int c2 = 0;
         String concat = "";
+
         for (String s : input) {
+            //Formatting input
             Map<String, Integer> digit = new HashMap<>();
             String[] all = s.split(" \\| ");
             String[] signalPattern = all[0].split(" ");
@@ -30,8 +33,10 @@ public class Day8 extends AOCHandler {
 			/*
 			len: 2,3,4,7  -> digit: 1, 4, 7, 8
 			     5        ->           2, 3, 5,
-			     6        ->           6, 9 rest
+			     6        ->           6, 9, 0 rest
 			 */
+
+            //Assigning digits with single length
             List<String> pattern = Arrays.asList(signalPattern);
             List<String> copy = new ArrayList<>(List.copyOf(pattern));
             for (int i = 0; i < pattern.size(); i++) {
@@ -54,29 +59,44 @@ public class Day8 extends AOCHandler {
                     }
                 }
             }
+
             Map<Integer, String> reversed = new HashMap<>();
             for (Map.Entry<String, Integer> entry : digit.entrySet()) {
                 reversed.put(entry.getValue(), entry.getKey());
             }
+
+            //Sorting to use longer signalpatterns to define 0,6,9
+            copy = copy.stream().sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+            Collections.reverse(copy);
+
             for (String rest : copy) {
                 if (rest.length() == 5) {
                     if (rest.contains(String.valueOf(reversed.get(1).charAt(0))) && rest.contains(String.valueOf(reversed.get(1).charAt(1)))) {  //contains both chars from 1
                         digit.put(rest, 3);
                     } else {
                         //if rest contains any letter of a 6
-                        var temp = sortString(reversed.get(1));
-                        if (rest.contains(String.valueOf(temp.charAt(0)))) {
-                            digit.put(rest, 2);
-                        } else {
+                        boolean b = allThreeInCommon(reversed.get(1), reversed.get(6), rest);
+                        if (b) {
                             digit.put(rest, 5);
+                        } else {
+                            digit.put(rest, 2);
                         }
                     }
                 } else if (rest.length() == 6) {
+                    //diff between 0 and 9
                     if (rest.contains(String.valueOf(reversed.get(1).charAt(0))) && rest.contains(String.valueOf(reversed.get(1).charAt(1)))) {
-                        digit.put(rest, 9);
-                    } else {
+                        //contains a whole 4? -> 9
+                        if (rest.contains(String.valueOf(reversed.get(4).charAt(0))) && rest.contains(String.valueOf(reversed.get(4).charAt(1))) && rest.contains(String.valueOf(reversed.get(4).charAt(2))) && rest.contains(String.valueOf(reversed.get(4).charAt(3)))) {
+                            digit.put(rest, 9);
+                        } else {
+                            digit.put(rest, 0);
+                        }
+                    } else if (rest.contains(String.valueOf(reversed.get(1).charAt(0))) ^ rest.contains(String.valueOf(reversed.get(1).charAt(1)))) {
                         digit.put(rest, 6);
                     }
+                }
+                for (Map.Entry<String, Integer> entry : digit.entrySet()) {
+                    reversed.put(entry.getValue(), entry.getKey());
                 }
             }
 
@@ -91,16 +111,26 @@ public class Day8 extends AOCHandler {
                     }
                 }
             }
-            System.out.println(concat);
+
             c2 += Integer.parseInt(concat);
             concat = "";
+
         }
+
         System.out.println("Part 1: " + c);
         System.out.println("Part 2: " + c2);
     }
 
+    private boolean allThreeInCommon(String s, String s1, String rest) {
+        boolean three = false;
+        if ((s1.contains(String.valueOf(s.charAt(0))) && rest.contains(String.valueOf(s.charAt(0)))) || (s1.contains(String.valueOf(s.charAt(1))) && rest.contains(String.valueOf(s.charAt(1))))) {
+            three = true;
+        }
+        return three;
+    }
+
     public static String sortString(String inputString) {
-        char tempArray[] = inputString.toCharArray();
+        char[] tempArray = inputString.toCharArray();
         Arrays.sort(tempArray);
         return new String(tempArray);
     }
